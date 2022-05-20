05f83126f369167102d1bb29900606f462f8d424
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-05-20 21:16:57 +0300

    Time: 165 ms (29.20%), Space: 15.5 MB (88.11%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -2,6 +2,7 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         indegree = [0] * numCourses
         neighbors = defaultdict(list)
+        counter = 0
         for pre in prerequisites:
             indegree[pre[0]]+=1
             neighbors[pre[1]].append(pre[0])
@@ -9,13 +10,14 @@ class Solution:
         for i in range(len(indegree)):
             if indegree[i] == 0:
                 queue.append(i)
+                counter+=1
         while queue:
             node = queue.popleft()
             for i in neighbors[node]:
                 indegree[i] -= 1
                 if indegree[i] == 0:
                     queue.append(i)
-        if max(indegree) != 0:
-            return False
-        return True
+                    counter +=1
+        if counter == numCourses:
+            return True
         
