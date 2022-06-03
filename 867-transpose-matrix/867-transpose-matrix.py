cc57868c3c6a0a9585e9ddbeea97a9ee242baf67
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-06-03 17:11:19 +0300

    Time: 111 ms (36.31%), Space: 14.7 MB (55.25%) - LeetHub

---
+++ 867-transpose-matrix/867-transpose-matrix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        transpose = []
+        for i in range(len(matrix[0])):
+            temp = []
+            for j in range(len(matrix)):
+                temp.append(matrix[j][i])
+            transpose.append(temp)
+        # print(transpose)
+        return transpose
