797dcb44846c265aa03863d84035a6c1a7a6c4da
Biniyam Demissew <39224205+biniyam112@users.noreply.github.com>
2022-08-02 21:22:24 +0300

    Time: 67 ms (11.40%), Space: 14.1 MB (10.96%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,5 +1,19 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return factorial(m+n-2)//factorial(m-1)//factorial(n-1)
+        dirs = [[0,1],[1,0]]
+        inbound = lambda row , col : 0 <= row < m and 0 <= col < n
+        visited = {}
+        def dfs(row,col):
+            if row == m-1 or col == n-1:
+                return 1
+            if (row,col) not in visited:
+                value = 0
+                for way in dirs:
+                    new_row,new_col = row+way[0],col+way[1]  
+                    if inbound(new_row,new_col):
+                        value += dfs(new_row,new_col)
+                visited[(row,col)] = value
+            return visited[(row,col)]
+        return dfs(0,0)
             
         
