e3d83cd3c8fabb765f1219f591e8854ea0ee31e4
biniyam112 <biniyamdemissew112@gmail.com>
2022-09-19 12:12:15 +0300

    Revert "n"
    This reverts commit 24ffec8dc38aca9384d5d46893f9bc93552439fb.

--- contest/contest 21/dima and staircase.py
+++
@@ -1,9 +0,0 @@
-strairs = int(input())
-strairHeight = input().split()
-strairHeight = [int(x) for x in strairHeight]
-boxes = int(input())
-boxesDimention = []
-for _ in range(boxes):
-    temp = input().split()
-    boxesDimention.append([int(temp[0]), int(temp[1])])
-print(boxesDimention)
--- contest/contest 28/Mahmoud and Longest Uncommon Subsequence.py
+++
@@ -1,6 +0,0 @@
-a = input()
-b = input()
-if a != b:
-    print(max(len(a), len(b)))
-else:
-    print(-1)
